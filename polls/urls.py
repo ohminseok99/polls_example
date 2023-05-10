f6356416742140dd068d99368fce4bf39c685fcc
unknown <alstjr4851@naver.com>
2023-05-10 15:52:54 +0900

    Initial commit

---
+++ polls/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from polls import views
+from django.urls import path, include
+
+app_name = 'polls'
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('/<int:question_id>/', views.detail, name='detail'),
+    path('/<int:question_id>/vote/', views.vote, name='vote'),
+    path('/<int:question_id>/results/', views.results, name='result'),
+]
